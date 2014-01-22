3138a140b3821377d59a7d1ce5a9d774be5659c5
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-01-21 18:38:16 -0800

    Item's should be under the hatsrank module
    Change-Id: I04ec067d6a891e3b68b62ba676cc756d0608b649

--- lib/hatsrank/item.rb
+++ lib/hatsrank/item.rb
@@ -1,10 +1,12 @@
-class Item
-  attr_accessor :name, :descriptions
-  def initialize
-    @descriptions = []
-  end
+module Hatsrank
+  class Item
+    attr_accessor :name, :descriptions
+    def initialize
+      @descriptions = []
+    end
 
-  def effects
-    descriptions.select { |x| x.value.match /^Effect/ }
+    def effects
+      descriptions.select { |x| x.value.match /^Effect/ }
+    end
   end
 end
